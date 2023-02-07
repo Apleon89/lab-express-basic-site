0dbd1e7df9d520c25d2a4844f43661501d9b07bd
Alberto Pérez <apleon89@gmail.com>
2023-02-07 17:02:57 +0000

    style home done

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 app.get('/home', (req, res) => {
   res.sendFile(__dirname + "/views/home.html")
 })
@@ -13,6 +15,10 @@ app.get('/about', (req, res) => {
 app.get("/works", (req,res)=>{
   res.sendFile(__dirname + "/views/works.html")
 })
+
+app.use(function(req, res, next) {
+    res.status(404).send('Lo sentimos, esta página no está en nuestra JorgeAgenda!!');
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
