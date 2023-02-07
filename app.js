7ac8fcc53f4f9f4f23407f41953f933d0289efa2
Alberto Gómez <albertogomrod5@gmail.com>
2023-02-07 17:23:08 +0100

    Html about y work, hechos

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get('/home', (req, res) => {
   res.sendFile(__dirname + "/views/home.html")
 })
 
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + "/views/about.html")
+})
+
+app.get("/works", (req,res)=>{
+  res.sendFile(__dirname + "/views/works.html")
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
